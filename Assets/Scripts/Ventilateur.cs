ddf7c83dc7fc87295b4a8ccbf01b3a694d4a5844
Sabri <sabribenbrahim@outlook.com>
2019-01-26 22:38:30 +0100

    Script Ventilateur

---
+++ Assets/Scripts/Ventilateur.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ventilateur : MonoBehaviour
+{
+	float venti = 0f;
+	float variableAdditionnel = 15f;
+	float variableSoustrait = 4f;
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (venti > 0)
+		{
+			venti -= (variableSoustrait * Time.deltaTime);
+			transform.rotation *= Quaternion.AngleAxis(venti, Vector3.up);
+		}
+	}
+
+	void OnMouseDown()
+			{
+					venti += variableAdditionnel;
+					transform.rotation *= Quaternion.AngleAxis(venti, Vector3.up);
+			}
+}
