79727ed0b8ca1f2a56796c3144ea3eaa3503df27
Benoit Fouletier <benoit@swingswingsubmarine.com>
2019-01-26 19:54:24 +0100

    test Pickable

---
+++ Assets/inventory/Pickable.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickable : BaseInteractable
+{
+	public enum State
+	{
+		Idle,
+		Dragging,
+	}
+	public State state;
+
+	public float distanceFromCamera = 5;
+	public float distanceFromCollision = 1;
+	public float moveSpeed = 1;
+	public bool debugHit;
+
+	public bool doubleSmooth;
+	public float smoothSpeed = 0.1f;
+	public float smoothSpeed1 = 0.1f;
+	public float smoothSpeed2 = 0.1f;
+
+	public Vector3 targetPos;
+	public Vector3 initialPosition;
+	public Vector3 initialScreenPosition;
+	public Vector3 lastScreenPosition;
+
+	protected override void onInteract()
+	{
+		base.onInteract();
+		//Debug.Log($"onInteract {state} {name}");
+
+		switch (state)
+		{
+			case State.Idle:
+			{
+				state = State.Dragging;
+				collider.enabled = false;
+				gameObject.layer = Physics.IgnoreRaycastLayer;
+
+				initialPosition = transform.position;
+				initialScreenPosition = Camera.main.WorldToScreenPoint(initialPosition);
+				lastScreenPosition = initialScreenPosition;
+				break;
+			}
+			case State.Dragging:
+			{
+				break;
+			}
+			default: throw new ArgumentOutOfRangeException();
+		}
+	}
+
+	protected override void Update()
+	{
+		base.Update();
+
+		if (state != State.Dragging)
+		{
+			return;
+		}
+
+		var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		Vector3 finalTargetPos;
+		if (Physics.Raycast(ray, out var hit))
+		{
+			finalTargetPos = hit.point - ray.direction * distanceFromCollision;
+			if (debugHit)
+			{
+				Debug.DrawLine(hit.point, hit.point + hit.normal, Color.red, 0.1f);
+			}
+		}
+		else
+		{
+			finalTargetPos = ray.GetPoint(initialScreenPosition.z);
+		}
+		lastHit = new SerializableRaycastHit(hit);
+
+		if (doubleSmooth)
+		{
+			targetPos += (finalTargetPos - targetPos) * smoothSpeed1;
+			transform.position += (targetPos - transform.position) * smoothSpeed2;
+		}
+		else
+		{
+			transform.position += (finalTargetPos - transform.position) * smoothSpeed;
+		}
+
+		//transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+		//transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, moveSpeed * Time.deltaTime);
+	}
+
+	public SerializableRaycastHit lastHit;
+	/*void OnDrawGizmos()
+	{
+		if (!lastHit.collider)
+		{
+			return;
+		}
+
+		var hit = lastHit;
+		Debug.DrawLine(hit.point, hit.normal, Color.red, 0.1f);
+	}*/
+}
---
+++ Assets/inventory/SetupScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class SetupScene : BaseMonoBehaviour
+{
+#if UNITY_EDITOR
+	[ContextMenu("Create Colliders")]
+	protected void createColliders()
+	{
+		foreach (var renderer in GetComponentsInChildren(typeof(MeshRenderer)))
+		{
+			if (!renderer.GetComponent<MeshCollider>())
+			{
+				Undo.AddComponent<MeshCollider>(renderer.gameObject);
+			}
+		}
+	}
+#endif
+}
