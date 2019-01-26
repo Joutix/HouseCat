a6e90fd436973300614eb4c219b57fa7fe7e4f70
Sabri <sabribenbrahim@outlook.com>
2019-01-26 16:46:54 +0100

    Mise A Jour 26/01
    Correctif du ScriptTableau : looping réalisé
    Ajout de son et la fonction Lampe
    Ajout de scène

---
+++ Assets/inventory/InventoryItem.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryItem : BaseInteractable
+{
+	public enum State
+	{
+		Idle,
+		InInventory,
+		Dragging,
+	}
+	public State state;
+	public int slotIndex = -1;
+
+	protected override void onInteract()
+	{
+		base.onInteract();
+		//Debug.Log($"onInteract {state} {name}");
+
+		switch (state)
+		{
+			case State.Idle:
+			{
+				state = State.InInventory;
+				inventory.addItem(this);
+				break;
+			}
+			case State.InInventory:
+			{
+				state = State.Dragging;
+				collider.enabled = false;
+				//gameObject.layer = Physics.IgnoreRaycastLayer;
+				inventory.selectItem(this);
+				break;
+			}
+			case State.Dragging:
+			{
+				break;
+			}
+			default: throw new ArgumentOutOfRangeException();
+		}
+	}
+
+	public float distanceFromCamera = 5;
+	protected override void Update()
+	{
+		base.Update();
+
+		if (state != State.Dragging)
+		{
+			return;
+		}
+
+		var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		transform.position = ray.GetPoint(distanceFromCamera);
+	}
+}
