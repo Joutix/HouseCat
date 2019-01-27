9a9f8011d3585b14f8cb1df7b82b0172d55a7fb7
Benoit Fouletier <benoit@swingswingsubmarine.com>
2019-01-27 15:38:04 +0100

    nullcheck conditionAfter

--- Assets/__scripts/ObjetInteraction.cs
+++ Assets/__scripts/ObjetInteraction.cs
@@ -48,6 +48,9 @@ public class ObjetInteraction : BaseInteractable
 
 		active = true;
 		director.Play();
-		conditionAfter.SetActive(true);
+		if (conditionAfter)
+		{
+			conditionAfter.SetActive(true);
+		}
 	}
 }
